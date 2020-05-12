f1163c8fa6c49aec6c96037a42b75d8a1ff88b17
Patrick Poul Nielsen <patr9244@edu.easj.dk>
2020-05-12 09:57:21 +0200

    test

---
+++ EDMXapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EDMXapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
